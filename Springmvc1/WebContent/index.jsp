40d323321b972848a87fe04303dd95521515768c
deepak <deepak@deepak-PC>
2019-10-21 21:42:31 +0530

    dss

---
+++ Springmvc1/WebContent/index.jsp
@@ -0,0 +1,1 @@
+<a href="add">Add Record</a>
---
+++ Springmvc1/src/mypack/MyController.java
@@ -0,0 +1,21 @@
+package mypack;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MyController implements Controller
+{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception 
+	{
+		ModelAndView mv=new ModelAndView("display");
+		mv.addObject("msg","welcome user");
+		
+	
+		return mv;
+	}
+
+
+}
